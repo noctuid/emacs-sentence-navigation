2a04ab0061dbaa0c09221a7bb42bdc4ee37b867a
angelic-sedition <nocturnalartifice@gmail.com>
2015-05-02 01:51:17 -0400

    Pull regex out of commands and use rx as suggested by vermiculus and gottabeme
    - Name regexes and use rx to make them more readable
    - Consolidate common regex patterns;
      still could be cleaner.. but it's an improvement
    - Further clarify sentence definition in readme
    - Fix handling of nested quotations (partially addresses #2)
    - Remove need for cl-subseq

--- sentence-navigation.el
+++ sentence-navigation.el
@@ -27,69 +27,112 @@
 (defun sn/reload-non-sentence-regex ()
   (setq sn--not-a-sentence
         (concat
-         " [\(\"'`“]?\\("
+         (rx (or bol " ") (0+ (in "\(\"'`“")))
+         "\\("
          (cl-reduce #'(lambda (x y) (concat x "\\|" y)) sn/abbreviation-list)
-         "\\)\\. ")))
+         "\\)"
+         ;; optional so will work at end or beginning of sentence
+         (rx (optional ". ")))))
 
 (sn/reload-non-sentence-regex)
 
-(defvar sn--maybe-sentence-regex
-  "\\(\\.[\"”`]?  ?\\|^[[:space:]]*\\)[[:upper:]\"“`]")
-(defvar sn--maybe-sentence-end "\\.[\"”`]?")
-(defvar sn--maybe-after-sentence-end-regex "\\.[\"”`]?\\( \\|$\\)")
+;; as suggested by emacs wiki
+(defmacro sn--rx-extra (&rest body-forms)
+   (let ((add-ins (list
+                   ;; wish could use previous ones..
+                   `(left-quotes . ,(rx (in "\"“`'")))
+                   `(sentence-end-char . ,(rx (in ".\"”`'")))
+                   `(0+-left-quotes . ,(rx (0+ (in "\"“`'"))))
+                   `(0+-right-quotes . ,(rx (0+ (in "\"”`'"))))
+                   `(maybe-sentence-start . ,(rx (0+ (in "\"“`'"))
+                                                 upper))
+                   `(maybe-sentence-end . ,(rx "." (0+ (in "\"”`'"))))
+                   `(bol-ignoring-ws . ,(rx bol (0+ space))))))
+     `(let ((rx-constituents (append ',add-ins rx-constituents nil)))
+        ,@body-forms)))
 
+;; regex for searching forward/backward
+(defvar sn--maybe-sentence-search
+      (sn--rx-extra (rx (or
+                         (and maybe-sentence-end " " (optional " "))
+                         (and bol (0+ space)))
+                        maybe-sentence-start)))
+
+(defvar sn--maybe-sentence-end-search
+      (sn--rx-extra (rx maybe-sentence-end
+                        (or
+                         (and " " (optional " ")
+                              maybe-sentence-start)
+                         (and (0+ space) eol)))))
+
+;; helpers for correcting positioning
+(defvar sn--maybe-sentence-start (sn--rx-extra (rx maybe-sentence-start)))
+(defvar sn--maybe-sentence-end (sn--rx-extra (rx maybe-sentence-end)))
+
+(defun sn--maybe-at-bol-sentence-p ()
+  (let ((case-fold-search nil))
+    (and (looking-back (sn--rx-extra (rx bol-ignoring-ws)))
+         (looking-at sn--maybe-sentence-start))))
+
+(defun sn--maybe-before-sentence-start-p ()
+  (let ((case-fold-search nil))
+    (and
+     (looking-at sn--maybe-sentence-start)
+     (looking-back (rx (or " " bol))))))
+
+(defun sn--maybe-at-sentence-end-p ()
+  (looking-at (sn--rx-extra (rx sentence-end-char (or " " eol)))))
+
+(defun sn--at-sentence-end-p ()
+  (let ((case-fold-search nil))
+    (and (sn--maybe-at-sentence-end-p)
+         (not (looking-back sn--not-a-sentence)))))
+
+;; actual commands
 (defun sn/forward-sentence (&optional arg)
   (interactive)
   (dotimes (_ (or arg 1))
     ;; save point in case there is no match
     (point-to-register 'sn-saved-point)
     ;; so won't stay on current sentence if at beginning of line
-    (when (and (looking-at "^[[:space:]]*[[:upper:]\"“`]")
-               (looking-at "[[:upper:]\"“`]"))
+    (when (sn--maybe-at-bol-sentence-p)
       (right-char))
-    (while (progn
-             (let ((case-fold-search nil))
-               ;; move back so that don't skip next sentence if right before it
-               (while (looking-back sn--maybe-sentence-end)
-                 (left-char))
-               ;; don't move at all if search fails
-               (unless (re-search-forward sn--maybe-sentence-regex nil t)
-                 (jump-to-register 'sn-saved-point)
-                 (return nil))
-               (while (not (and (looking-at "[[:upper:]\"“`]")
-                                (looking-back "\\(\\.[\"”`]?  ?\\|^[[:space:]]*\\)")))
-                 (left-char)))
+    (while (let ((case-fold-search nil))
+             ;; move back so that don't skip next sentence if right before it
+             (while (looking-back sn--maybe-sentence-end)
+               (left-char))
+             ;; don't move at all if search fails
+             (unless (re-search-forward sn--maybe-sentence-search nil t)
+               (jump-to-register 'sn-saved-point)
+               (return nil))
+             (while (not (sn--maybe-before-sentence-start-p))
+               (left-char))
              (looking-back sn--not-a-sentence)))))
 
 (defun sn/forward-sentence-end (&optional arg)
   (interactive)
   (dotimes (_ (or arg 1))
     (point-to-register 'sn-saved-point)
-    (while (progn
+    (while (let ((case-fold-search nil))
              ;; move to start of next possible sentence
              ;; if already at end of current or after an abbrev
-             (when (and (looking-at "[\\.”\"`]")
-                        ;; " is ambigious for sentence start or end
-                        (not (looking-at "[\"`][[:upper:]]")))
-               (re-search-forward sn--maybe-sentence-regex nil t))
-             (unless (re-search-forward sn--maybe-after-sentence-end-regex nil t)
+             (when (sn--at-sentence-end-p)
+               (re-search-forward sn--maybe-sentence-start nil t))
+             (unless (re-search-forward sn--maybe-sentence-end-search nil t)
                (jump-to-register 'sn-saved-point)
                (return nil))
-             (while (not (looking-at "[\\.\"”`]"))
+             (while (not (sn--maybe-at-sentence-end-p))
                (left-char))
-             (looking-back (concat (cl-subseq sn--not-a-sentence 0 -1) "?"))))))
+             (looking-back sn--not-a-sentence)))))
 
 (defun sn/backward-sentence (&optional arg)
   (interactive)
   (dotimes (_ (or arg 1))
-    (while (progn
-             (let ((case-fold-search nil))
-               (unless (re-search-backward sn--maybe-sentence-regex nil t)
-                 (return nil))
-               (while (and
-                       (looking-at "[\\.\"”`[:space:]]")
-                       (not (looking-at "[\"“`][[:upper:]]")))
-                 (right-char)))
+    (while (let ((case-fold-search nil))
+             (unless (re-search-backward sn--maybe-sentence-search nil t)
+               (return nil))
+             (while (not (looking-at sn--maybe-sentence-start))
+               (right-char))
              (looking-back sn--not-a-sentence)))))
 
 (defun sn/backward-sentence-end (&optional arg)
@@ -97,16 +140,16 @@
   (dotimes (_ (or arg 1))
     (point-to-register 'sn-saved-point)
     ;; move forward so don't skip prevous sentence if right in front of it
-    (while (and (looking-at "[[:upper:]\"“`]")
-                (not (looking-at "[\"`]$")))
+    (while (let ((case-fold-search nil))
+             (looking-at sn--maybe-sentence-start))
       (right-char))
-    (while (progn
-             (unless (re-search-backward sn--maybe-after-sentence-end-regex nil t)
+    (while (let ((case-fold-search nil))
+             (unless (re-search-backward sn--maybe-sentence-end-search nil t)
                (jump-to-register 'sn-saved-point)
                (return nil))
-             (when (looking-at "\\.[\"”`]")
+             (while (not (sn--maybe-at-sentence-end-p))
                (right-char))
-             (looking-back (cl-subseq sn--not-a-sentence 0 -3))))))
+      (looking-back sn--not-a-sentence)))))
 
 ;; add evil motions and text-objects if evil exists
 (when (require 'evil nil :noerror)
@@ -136,15 +179,5 @@
 
   (evil-define-text-object sn/evil-inner-sentence (count &optional beg end type)
     (evil-select-inner-object 'sn/evil-inner-sentence beg end type count)))
-
-;; (defun sn/start-of-sentence-p ()
-;;   (and (looking-back "\\(\\.  ?\\|^[[:space:]]*\\)")
-;;        (looking-at "[[:upper:]\"“`]")
-;;        (not (looking-back sn/not-a-sentence))))
-
-;; (defun sn/sentence-begin ()
-;;   (interactive)
-;;   (unless (sn/start-of-sentence-p)
-;;     (sn/backward-sentence)))
 
 (provide 'sentence-navigation)
